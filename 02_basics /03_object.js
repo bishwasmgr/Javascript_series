c492413c117c70f7b8596a89cbb507b2fbadc955
Bishwas Thapa Magar <54845910+bishwasmgr@users.noreply.github.com>
2024-11-10 14:06:08 +0000

    symbol and object

--- 02_basics /03_object.js
+++ 02_basics /03_object.js
@@ -14,10 +14,21 @@ const JsUser = {
 
 
 }
+// Always use square notation if you want to access the symbol and always define symbol before using it in Object
+JsUser.greeting = function(){
+    console.log(`Hello ${this.name}, Welcome to this Project. Your Key is ${this[mySym]}`)
 
-console.log(JsUser[mySym])
+}
+console.log(JsUser.greeting())
 
- 
+// console.log(JsUser[mySym])
+
+// JsUser.email = "bishwas4@outlook.com"
+
+// Object.freeze(JsUser)
+
+// console.log(JsUser);
+
 
 myArray = ["h", "i"]
 // there is only methond to access element just by the index value
